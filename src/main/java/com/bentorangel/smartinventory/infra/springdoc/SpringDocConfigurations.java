5c39c6cc91bc7bd5e73c2a1fc82ab5b09b1144b2
Bento <bento.rangel05@gmail.com>
2026-03-24 20:31:10 -0300

    docs: configure JWT authentication and custom info in Swagger UI

---
+++ src/main/java/com/bentorangel/smartinventory/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,33 @@
+package com.bentorangel.smartinventory.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Smart Inventory API")
+                        .description("API Rest para controle de estoque inteligente, contendo funcionalidades de CRUD, auditoria de movimentações e autenticação via JWT.")
+                        .contact(new Contact()
+                                .name("Bento Rangel")
+                                .url("https://github.com/bentors/smart-inventory-api"))
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+    }
+}
